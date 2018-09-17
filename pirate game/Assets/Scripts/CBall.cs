cd3c5d191f89b24c6a9fff54fb662564f72db6d0
Thorgodof4 <cuatro.smith.4@gmail.com>
2018-09-17 11:30:38 -0400

    to fix bug

---
+++ pirate game/Assets/Scripts/CBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CBall : MonoBehaviour {
+
+	public GameObject Splash;
+	public float CballTime;
+
+	float HalfTime;
+	float UpTime = 0;
+	float time = 0;
+	
+
+	void Start () {
+		HalfTime = CballTime / 2;
+	}
+
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time < HalfTime) {
+			UpTime += Time.deltaTime;
+		} else {
+			UpTime -= Time.deltaTime;
+		}
+
+		this.gameObject.transform.localScale = new Vector3(0.5f + UpTime, 0.5f + UpTime, 0.5f);
+		
+		if (time >= CballTime) {
+			Instantiate (Splash, transform.position, Quaternion.Euler(0,0,0));
+			Destroy (gameObject);
+		}
+	}
+}
